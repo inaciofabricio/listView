55f9ec5896980006f7ff83cc40fea746b14951e1
Inácio Fabrício Nunes de Sousa <fabricionunes.si@hotmail.com>
2018-09-18 15:20:15 -0300

    Primeiro commit

---
+++ app/src/main/java/listview/cursoandroid/com/listview/MainActivity.java
@@ -0,0 +1,44 @@
+package listview.cursoandroid.com.listview;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private ListView listaItens;
+    private String[] itens = {"Campina Grande", "João Pessoa", "Patos", "Sumé", "Pombal", "Soledade", "Livramento", "Desterro"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listaItens = (ListView) findViewById(R.id.listViewId);
+
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                itens
+        );
+
+        listaItens.setAdapter(adaptador);
+
+        listaItens.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int codigoPosicao = position;
+                String valorClicado = listaItens.getItemAtPosition(codigoPosicao).toString();
+                Toast.makeText(getApplicationContext(), valorClicado, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
